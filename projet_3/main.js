2a20c0a59cdaf5f17a12f36e9cf5bdff41337f29
sallartiste <sallartiste@gmail.com>
2018-02-02 14:44:04 +0100

    je suis malade

--- projet_3/main.js
+++ projet_3/main.js
@@ -12,3 +12,25 @@ setInterval(function(){
             vm.rebours.push(counter--)
       }
 },1000)
+
+/*    V_ON = Evenement ecouté par Vue     */
+ let vn = new Vue ({
+       el:'#app2',
+       methods:{
+             toast(){
+                   this.toastedPain++
+             }
+       },
+       data:{
+           toastedPain:0,
+           worlds: ['Terran', 'Julie', 'New kroy','vaste']
+       }
+ });
+
+ new Vue ({
+     el:'#app3',
+     data:{
+         noms:['Jean','Pierre','Luc','Thomas'],
+         newName: ''
+     }
+ })
