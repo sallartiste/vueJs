3d720ef7e0d901ca96a19507e939edf30b9f7e85
sallartiste <sallartiste@gmail.com>
2018-02-06 16:54:06 +0100

    insta node

--- vue2/pro_1/vue-2/main.js
+++ vue2/pro_1/vue-2/main.js
@@ -1,6 +1,8 @@
 new Vue({
     el:'#app',
     data:{
-        groen:"Bonjour le ciel"
+        groen:"Bonjour le ciel",
+        date:Date.now(),
+        url:'https://img00.deviantart.net/a753/i/2017/180/9/5/black_panther_licensing_art_by_joeyvazquez-dbehs2w.jpg'
     }
 })
