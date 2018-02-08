c6146c0dc017c8bebf157460efcbae28e4e230a2
sallartiste <sallartiste@gmail.com>
2018-02-08 10:55:48 +0100

    vueJs events

--- vue2/pro_1/vue-2/main.js
+++ vue2/pro_1/vue-2/main.js
@@ -3,6 +3,7 @@ new Vue({
     data:{
         groen:"Bonjour le ciel",
         date:Date.now(),
-        url:'https://img00.deviantart.net/a753/i/2017/180/9/5/black_panther_licensing_art_by_joeyvazquez-dbehs2w.jpg'
+        url:'https://img00.deviantart.net/a753/i/2017/180/9/5/black_panther_licensing_art_by_joeyvazquez-dbehs2w.jpg',
+        name:''
     }
 })
