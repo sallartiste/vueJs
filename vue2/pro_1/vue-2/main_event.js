33a10a3cae354c68c14a4184330b69b17ebc12f9
sallartiste <sallartiste@gmail.com>
2018-02-09 13:41:24 +0100

    commit du jour

--- vue2/pro_1/vue-2/main_event.js
+++ vue2/pro_1/vue-2/main_event.js
@@ -1,4 +1,4 @@
-new Vue({
+let vm = new Vue({
     el:'#app',
     data:{
         title:'A faire',
@@ -7,9 +7,18 @@ new Vue({
     },
     methods:{
         addTask: function(){
-            this.tasks.push({id: Date.now(), description: this.task, done:false});
-            console.log('tasks', this.tasks);
-            this.task = '';
+            this.tasks.push({id: Date.now(), description: this.task, done:false, ifIsEditMode:false});
+            //console.log('tasks', this.tasks);
+            this.task = null;
+        },
+        switchMode: function(task){
+            task.ifIsEditMode = !task.ifIsEditMode;
+            //console.log(task);
+        },
+        deleteTask: function(task){
+            this.tasks = this.tasks.filter(function(t){
+                return t.id !== task.id;
+            })
         }
     }
 })
