a8114658359ca3a6725d8413bc02f12681eeff65
sallartiste <sallartiste@gmail.com>
2018-02-12 00:26:31 +0100

    creation des composant vueJs

--- vue2/pro_1/vue-2/main_comp.js
+++ vue2/pro_1/vue-2/main_comp.js
@@ -1,15 +1,32 @@
+Vue.component('global-cmp', {
+    template: '<div>GLOBAL component</div>'
+});
+
+let myCmp = {
+    //ajout des données
+    data: function(){
+        return {
+            randomNumber: Math.floor(Math.random() * 10)
+        }
+    },
+
+    template:'<p>LOCAL component: {{randomNumber}} <button v-on:click="refrefhNumber()">F5</button> viens du parent {{msg}}</p>',
+
+    //ajout des methodes
+    methods: {
+      refrefhNumber() {
+        this.randomNumber = Math.floor(Math.random() * 10)
+      }
+    },
+    props: ['msg']
+};
+
 let vm = new Vue({
     el:'#app',
-    data:{
-        title:'lifecycle hooks',
-        usersUrl:'https://jsonplaceholder.typicode.com/users',
-        users:[]
-    },
-    created:function(){
-        axios.get(this.usersUrl)
-        .then(function(response){
-            vm.users = response.data;
-            console.log(vm.users);
-        })
-    }
+    components: {
+      'my-compo':myCmp
+  },
+  data:{
+    message:'Je suis ici'
+  }
 })
