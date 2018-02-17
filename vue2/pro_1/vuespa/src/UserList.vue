5861e4d5f28fa86bba768e25207dda55e38ceff9
sallartiste <sallartiste@gmail.com>
2018-02-18 00:17:25 +0100

    commit du soir

---
+++ vue2/pro_1/vuespa/src/UserList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="user in data">
+                <user-details :usr="user" v-on:accept="registerUser(user)"></user-details>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export defaut {
+      name: 'user-list',
+      components: {
+        'user-details': UserDetails
+    },
+    props: ['data','usersComing'],
+    methods:{
+        registerUser: function(user){
+            this.usersComing.push(user);
+        }
+    },
+    data: function(){
+      userWhoWillComing: []
+    }
+  }
+</script>
+
+<style></style>
